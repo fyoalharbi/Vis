3161ecfcd6002535c830bd9df798b47ffc35d933
fayha'a alharbi <fofo.kiot@gmail.com>
2024-01-09 02:52:18 +0300

    Added input validation

--- visualizer.js
+++ visualizer.js
@@ -5,6 +5,40 @@
 //create canvas, add input
 
 let x = [];
-x = document.querySelector('Textarea');
+x = document.getElementById('input');
+x = x.split(',');
+let quick = document.getElementById('quick');
+let bubble = document.getElementById('bubble');
+let linear = document.getElementById('linear');
+let sort = document.getElementById('confirm');
+let ErMes = document.getElementById('mes');
+let algo = document.querySelector('al');
 
-document.addEventListener()
+
+sort.addEventListener('click', () => {
+    if (x.length <= 0){
+        ErMes = "Enter Valid Numbers!";
+        alert("AA");
+    }
+    else{
+        ErMes ="";
+        switch(algo.value){
+            case "bubble sort":
+                alert("buble sort");
+            break;
+            case "quick sort":
+                alert("buble sort");
+            break;
+            case "linear sort":
+                alert("buble sort");
+                break;
+
+                default:
+                    break;
+                        
+        }
+
+        }
+    }
+);
+
