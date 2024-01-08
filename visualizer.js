4e633ec09244fc8a7437181b64384da24596f1eb
fayha'a alharbi <fofo.kiot@gmail.com>
2024-01-09 02:59:39 +0300

    Validator

--- visualizer.js
+++ visualizer.js
@@ -3,42 +3,78 @@
 //take input 
 
 //create canvas, add input
+document.addEventListener('DOMContentLoaded', function() {
 
-let x = [];
-x = document.getElementById('input');
-x = x.split(',');
 let quick = document.getElementById('quick');
 let bubble = document.getElementById('bubble');
 let linear = document.getElementById('linear');
 let sort = document.getElementById('confirm');
 let ErMes = document.getElementById('mes');
-let algo = document.querySelector('al');
-
+let algo = document.getElementById('al');
+//string -> list of integers
 
 sort.addEventListener('click', () => {
-    if (x.length <= 0){
-        ErMes = "Enter Valid Numbers!";
-        alert("AA");
+    let x = document.getElementById('input').value;
+    
+    x = x.split(','); // string -> list // 
+    let validity = x.every(n => {return ValidateInput(n)});
+    alert(validity);
+    if (validity === false){
+        alert("not a number");
+        ErMes.innerText = "Enter a number";
     }
     else{
-        ErMes ="";
+
+        alert("a number");
+        ErMes.innerText = "";
         switch(algo.value){
             case "bubble sort":
-                alert("buble sort");
+                
             break;
             case "quick sort":
-                alert("buble sort");
+                
             break;
             case "linear sort":
-                alert("buble sort");
-                break;
+            break;
 
-                default:
-                    break;
+            default:
+            break;
                         
         }
 
         }
     }
 );
+ }, false);
 
+
+function ValidateInput(n){
+    if(isNaN(n)){
+        alert("FALSE");
+        return false;
+    }
+    else{
+        alert("TRUE");
+        return true;
+    }
+}
+
+function setup() {
+    canvas = createCanvas(1000, 1000);
+}
+
+function draw(){
+
+}
+function CreateNodes(){
+
+}
+function linear(){
+
+}
+function quick(){
+
+}
+function bubble(){
+
+}
